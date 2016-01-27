49f53efa56b647c5e618a153cede3f8c6235ee88
Allison Keane Barr <allisonkeane@gmail.com>
2016-01-27 22:10:56 +0100

    desktop styles plus photo overlay

--- components/Application.jsx
+++ components/Application.jsx
@@ -1,12 +1,19 @@
 import {element} from 'deku'
 import cx from 'classnames'
+import marked from 'marked'
 
 const pageSelect = page => ({ type: 'SELECT_PAGE', page })
 const sidebarToggle = () => ({ type: 'TOGGLE_SIDEBAR' })
-const projectsNavToggle = () => ({ type: 'TOGGLE_PROJECTS_NAV'})
-const projectSelect = project => ({ type: 'SELECT_PROJECT', project})
+const projectSelect = project => ({ type: 'SELECT_PROJECT', project })
+const overlayClose = () => ({ type: 'CLOSE_OVERLAY' })
+const overlayOpen = image => ({ type: 'OPEN_OVERLAY', image })
 
-const createPhotoA = (url) => <a style={`background-image: url(${url})`}></a>
+const prevDef = fn => e => {
+  e.preventDefault()
+  fn()
+}
+
+const createPhotoA = (dispatch, url, i) => <a href="#" style={`background-image: url(${url})`} onClick={prevDef(() => dispatch(overlayOpen(i)))} ></a>
 
 export default {
   render({ context, dispatch }) {
@@ -14,6 +21,7 @@ export default {
       <div>
         <SideBar />
         <Content />
+        { context.view.overlayOpen ? <Overlay /> : null }
       </div>
     );
   }
@@ -22,23 +30,25 @@ export default {
 const SideBar = {
   render({ context, dispatch }) {
 
-    const getClasses = (page) => cx({ active: context.view.page === page })
-    const createNavLi = (text) => <li class={getClasses(text) + ' nav-item'} onClick={() => dispatch(pageSelect(text))}>{text}</li>
+    const getActiveClass = (subject, type) => cx({ active: context.view[type] === subject })
+    const createNavLi = (name) => <li class={getActiveClass(name, 'page') + ' nav-item'} onClick={() => dispatch(pageSelect(name))}>{name}</li>
+    const createProjectLi = (name) => <li class={getActiveClass(name, 'currentProject')} onClick={() => dispatch(projectSelect(name))}>{context.projects[name].title}</li>
 
     return <div id="sidebar-component" class={ context.view.sidebarHidden ? 'hidden' : '' }>
 
       <h1 id="nav-title">Andrew Barr Photography</h1>
 
-      <a id="nav-toggle" href="#" onClick={() => dispatch(sidebarToggle())}><i class="fa fa-bars"></i></a>
+      <a id="nav-toggle" href="#" onClick={() => dispatch(sidebarToggle())}><i class="material-icons">menu</i></a>
 
       <ul id="nav">
         { ['gallery', 'about'].map(createNavLi) }
-        <li class={getClasses('projects') + ' nav-item'} onMouseOver={() => dispatch(projectsNavToggle())}>Projects<i class="fa fa-angle-right"></i></li>
-      </ul>
-
-      <ul id="projects-nav" class={ context.view.projectsNavHidden ? 'hidden' : '' }>
-        <li onClick={() => dispatch(projectSelect('jfkHouse'))}>JFK House</li>
-        <li onClick={() => dispatch(projectSelect('duckHouse'))}>Duck House</li>
+        <li id="projects-link" class={getActiveClass('projects', 'page') + ' nav-item'} >
+          Projects
+          <i class="material-icons">chevron_right</i>
+          <ul id="projects-nav" class={ context.view.projectsNavHidden ? 'hidden' : '' }>
+            { Object.keys(context.projects).map(createProjectLi) }
+          </ul>
+        </li>
       </ul>
 
       <a id="contact" href="mailto:abarrphoto@gmail.com">abarrphoto@gmail.com</a>
@@ -61,11 +71,12 @@ const Content = {
 }
 
 const Gallery = {
-  render({ context }) {
+  render({ context, dispatch }) {
+
     return <div class="page-component" id="gallery-component">
       <h2 class="page-title">Gallery</h2>
       <div class="thumbs">
-        { context.imageUrls.map(createPhotoA) }
+        { context.galleryImages.map(createPhotoA.bind(null, dispatch)) }
       </div>
     </div>
   }
@@ -73,14 +84,15 @@ const Gallery = {
 
 
 const Projects = {
-  render({ context }) {
+  render({ context, dispatch }) {
+
     const current = context.projects[context.view.currentProject]
 
     return <div class="page-component project-component" id="projects-component">
       <h2 class="page-title">{ current.title }</h2>
-      <p>{ current.blurb }</p>
+      <div id="project-blurb" innerHTML={ marked(current.blurb) }></div>
       <div class="thumbs">
-        { current.photos.map(createPhotoA) }
+        { current.photos.map(createPhotoA.bind(null, dispatch)) }
       </div>
     </div>
   }
@@ -90,7 +102,38 @@ const About = {
   render({ context }) {
     return <div class="page-component" id="about-component">
       <h2 class="page-title">About</h2>
-      <p id="about-p">{ context.about }</p>
+      <div id="about-p" innerHTML={ marked(context.about) }></div>
+    </div>
+  }
+}
+
+const Overlay = {
+  render({ context, dispatch }) {
+
+    const i = context.view.currentImage
+    const currentImages = context.view.page === 'gallery' ?
+      context.galleryImages :
+      context.projects[context.view.currentProject].photos
+
+    const showNextImg = () => {
+      return currentImages[i+1] ?
+        dispatch(overlayOpen(i+1)) :
+        dispatch(overlayOpen(0))
+    }
+
+    const showPrevImg = () => {
+      return currentImages[i-1] ?
+        dispatch(overlayOpen(i-1)) :
+        dispatch(overlayOpen(currentImages.length-1))
+    }
+
+    return <div id="overlay-component">
+      <div id="overlay-container">
+        <i class="material-icons" id="left-arrow" onClick={showPrevImg}>chevron_left</i>
+        <div id="overlay-image-div" style={ 'background-image: url(' + currentImages[i] + ')'}></div>
+        <i class="material-icons" id="right-arrow" onClick={showNextImg}>chevron_right</i>
+      </div>
+      <i class="material-icons" id="close-button" onClick={() => dispatch(overlayClose())} >close</i>
     </div>
   }
 }
--- reducers.js
+++ reducers.js
@@ -1,28 +1,41 @@
 const view = (state, action) => {
   switch (action.type) {
     case 'SELECT_PAGE':
-      return Object.assign(state, { page: action.page, sidebarHidden: true, projectsNavHidden: true })
+      return Object.assign(state, {
+        page: action.page,
+        sidebarHidden: true,
+        currentProject: action.page === 'projects' ? state.currentProject : null
+      })
     case 'TOGGLE_SIDEBAR':
-      return Object.assign(state, { sidebarHidden: !state.sidebarHidden, projectsNavHidden: true })
-    case 'TOGGLE_PROJECTS_NAV':
-      return Object.assign(state, { projectsNavHidden: !state.projectsNavHidden })
+      return Object.assign(state, { sidebarHidden: !state.sidebarHidden })
     case 'SELECT_PROJECT':
-      return Object.assign(state, { page: 'projects', currentProject: action.project, sidebarHidden: true, projectsNavHidden: true })
+      return Object.assign(state, { page: 'projects', currentProject: action.project, sidebarHidden: true })
+    case 'OPEN_OVERLAY':
+      return Object.assign(state, { overlayOpen: true, currentImage: action.image })
+    case 'CLOSE_OVERLAY':
+      return Object.assign(state, { overlayOpen: false })
     default:
       return state || {
         page: 'gallery',
         sidebarHidden: true,
-        projectsNavHidden: true,
-        currentProject: null
+        currentProject: null,
+        overlayOpen: false,
+        currentImage: null
       }
   }
 }
 
-const imageUrls = () => {
+const galleryImages = () => {
   return [
   'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/01-West_Elevation_q65msi.jpg',
   'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/02-South_elevation_b0wgxk.jpg',
-  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/03-Southeast_Elevation_aihkbq.jpg'
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/03-Southeast_Elevation_aihkbq.jpg',
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/01-West_Elevation_q65msi.jpg',
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/02-South_elevation_b0wgxk.jpg',
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/03-Southeast_Elevation_aihkbq.jpg',
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/01-West_Elevation_q65msi.jpg',
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/02-South_elevation_b0wgxk.jpg',
+  'http://res.cloudinary.com/dv3yibyz2/image/upload/c_fit,h_200/_IGP4889_v9ltv8.jpg'
   ]
 }
 
@@ -33,13 +46,24 @@ const projects = () => {
       blurb: 'This dwelling, which is the last vacation home used by JFK and his family in the summer of 1963, is on Squaw Island in Hyannisport, MA. It was photographed for a National Historic Register Nomination, but the new owner, a Kennedy family member, declined to go forward with it.',
       photos: [
         'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//jfk-house/img001.jpg',
-        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//jfk-house/img014_zmpab5.jpg'
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//jfk-house/img014_zmpab5.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//jfk-house/img014_zmpab5.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//jfk-house/img014_zmpab5.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//jfk-house/img014_zmpab5.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_vertical-12_ntdm3h.jpg'
       ]
     },
     duckHouse: {
       title: 'Duck House',
-      blurb: 'blah',
-      photos: []
+      blurb: '<p>From the Fenway Civic Association website:</p><p>"On Agassiz Road, the parkway that links the East and West Fenway, sits a late 19th century building known as the Duck House.  This City-owned building, which has been boarded up and out of use since the 1980s, has the potential to serve as a community asset."</p><p>This building was photo documented with large format photography to assist a documentation class of the BAC.</p>',
+      photos: [
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_horizontal-1_oajvgf.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_horizontal-3_cpliwe.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_horizontal-4_zohcsm.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_vertical-12_ntdm3h.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_vertical-12_ntdm3h.jpg',
+        'http://res.cloudinary.com/dv3yibyz2/image//upload/c_fit,w_200,h_200//HABS_vertical-12_ntdm3h.jpg'
+      ]
     }
   }
 }
@@ -50,7 +74,7 @@ const about = () => {
 
 const reducers = {
   view,
-  imageUrls,
+  galleryImages,
   projects,
   about
 }
